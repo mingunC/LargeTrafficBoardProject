1e844453a97cc06676c80c9fad7d339d17a43fb2
mingunC <mingun.ryan.choi@gmail.com>
2025-01-01 15:34:43 -0500

    updated UserDTo, UserServiceImpl

--- src/main/java/com/cmgg919/boardserver/service/impl/UserServiceImpl.java
+++ src/main/java/com/cmgg919/boardserver/service/impl/UserServiceImpl.java
@@ -2,12 +2,24 @@ package com.cmgg919.boardserver.service.impl;
 
 import com.cmgg919.boardserver.Exception.DuplicateIdException;
 import com.cmgg919.boardserver.dto.UserDTO;
+import com.cmgg919.boardserver.mapper.UserProfileMapper;
 import com.cmgg919.boardserver.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.yaml.snakeyaml.constructor.DuplicateKeyException;
 
+import static com.cmgg919.boardserver.util.SHA256Util.encryptSHA256;
+
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserProfileMapper userProfileMapper;
+
+    public UserServiceImpl(UserProfileMapper userProfileMapper) {
+        this.userProfileMapper = userProfileMapper;
+    }
+
     @Override
     public void register(UserDTO userProfile) {
         boolean duplIdResult = isDuplicatedId(userProfile.getUserID());
@@ -15,6 +27,7 @@ public class UserServiceImpl implements UserService {
             throw new DuplicateIdException("This id is duplicated");
         }
         userProfile.setCreateTime(new java.util.Date());
+        userProfile.setPassword(encryptSHA256(userProfile.getPassword()));
     }
 
     private boolean isDuplicatedId(String userID) {
